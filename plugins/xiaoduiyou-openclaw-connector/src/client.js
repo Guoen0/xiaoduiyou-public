73a6ab1890f39dd9df5e6e853aede3ca795dc7c0
Guoen <sun.guoen0@gmail.com>
2026-06-01 21:51:22 +0800

    chore: bump OpenClaw connector version

--- plugins/xiaoduiyou-openclaw-connector/src/client.js
+++ plugins/xiaoduiyou-openclaw-connector/src/client.js
@@ -1,4 +1,4 @@
-export const XIAODUIYOU_CONNECTOR_VERSION = "2026.6.1";
+export const XIAODUIYOU_CONNECTOR_VERSION = "2026.6.2";
 
 async function readJsonResponse(response, path) {
   const rawText = await response.text();
